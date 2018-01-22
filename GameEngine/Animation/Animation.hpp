7aeef9925b0ffb04ba34efeaf8c84498158a8909
chauvi_d <antoine667@gmail.com>
2018-01-22 19:27:19 +0100

    Anim OK

---
+++ GameEngine/Animation/Animation.hpp
@@ -0,0 +1,26 @@
+//
+// Created by chauvin on 22/01/18.
+//
+
+#ifndef GAMEENGINE_ANIMATION_HPP
+#define GAMEENGINE_ANIMATION_HPP
+
+
+#include <Component/Component.hpp>
+
+class Animation : public TacosEngine::Component
+{
+ public:
+  Animation(const std::string &myname, const std::shared_ptr<TacosEngine::Sprite> &sprite, bool loop,
+	      int frame_speed, const std::vector<TacosEngine::ITexture*> &frames);
+  ~Animation() override = default;
+  void update(int tick);
+ private:
+  bool	_loop;
+  int	_frame_speed;
+  std::vector<TacosEngine::ITexture*> _frames;
+  int 	_frame;
+};
+
+
+#endif //GAMEENGINE_ANIMATION_HPP

c47dfc90aded8039b3b2d053808cb32856b37040
Zaan <bordes.corentin@gmail.com>
2018-01-24 14:30:44 +0100

    Game Engine v2

---
+++ GameEngine/SfmlRenderer/SfmlRenderer.hpp
@@ -0,0 +1,34 @@
+//
+// Created by chennetier on 14/01/18.
+//
+
+#ifndef R_TYPE_SFMLRENDERER_HPP
+#define R_TYPE_SFMLRENDERER_HPP
+
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <Scene/Scene.hpp>
+#include "IRenderer/IRenderer.hpp"
+
+namespace TacosEngine {
+
+    struct SortByLayout {
+        bool	operator()(const std::shared_ptr<GameObject> &i, const std::shared_ptr<GameObject> &j) const
+        {
+            return *i < *j;
+        }
+    };
+
+    class SfmlRenderer : public IRenderer {
+    private:
+        sf::RenderWindow *_window;
+
+    public:
+        explicit SfmlRenderer(sf::RenderWindow *window);
+        ~SfmlRenderer() override;
+        void draw(std::list<std::shared_ptr<GameObject>> _sprites) override;
+    };
+}
+
+
+#endif //R_TYPE_SFMLRENDERER_HPP
